4bebe1e61bfc8701dc5c2cbd3eb63c2f6970122d
Kevinmrozek <kmrozek0859@gmail.com>
2023-07-18 14:09:34 -0500

    commit

--- lib/db/debug.py
+++ lib/db/debug.py
@@ -1,4 +1,6 @@
 import ipdb
-from models import session, Puzzle
+from models import session, Puzzle, Choice, Situation
+
+p1 = session.query
 
 ipdb.set_trace()
--- lib/db/models.py
+++ lib/db/models.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Column, Integer, String, ForeignKey, MetaData, create_engine
-from sqlalchemy.orm import sessionmaker 
+from sqlalchemy.orm import sessionmaker, relationship
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.associationproxy import association_proxy
 
 convention = {
     'fk': 'fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s'
@@ -8,7 +9,7 @@ convention = {
 
 metadata = MetaData( naming_convention = convention)
 
-Base = declarative_base() # metadata = md
+Base = declarative_base(metadata = metadata) # metadata = md
 
 engine = create_engine('sqlite:///puzzle.db')
 Session = sessionmaker( bind = engine)
@@ -22,6 +23,9 @@ class Puzzle(Base):
     id = Column(Integer(), primary_key=True)
     name = Column(String())
 
+    choices = relationship('Choice')
+    
+
     def __repr__(self):
         return f'<Puzzle id: {self.id} name: {self.name}>'
 
@@ -30,3 +34,17 @@ class Choice(Base):
 
     id = Column(Integer(), primary_key=True)
     puzzle_id = Column(Integer(), ForeignKey('puzzles.id'))
+    #situaton.id = Column(Integer(), ForeignKey('situations.id'))
+
+    def __repr__(self):
+        return f'<Choice id: {self.id}'
+
+class Situation(Base):
+    __tablename__ = 'situations'
+
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
+
+    def __repr__(self):
+        return f'<Situation id: {self.id}'
+
