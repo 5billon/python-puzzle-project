c531f29ac4094c9053128dc7c0e4ad45db0f836e
Ashay Morwale <ashay123.m@gmail.com>
2023-07-20 17:53:55 -0400

    Finish MVP

--- lib/db/debug.py
+++ lib/db/debug.py
@@ -1,26 +1,48 @@
 import ipdb
 from models import session, Puzzle, Choice, Outcome
 
+
+print('press x to exit at any point. Type "Help" to see list of available commands')
 print('You walk into a cave you found, and run into a bunker.')
-print('\n Press c to continue or x to leave the bunker')
 
 user_input = 'not x'
-
-while user_input != 'x':
-    user_input = input('=>')
-    if user_input == 'c':
-        for puzzle in session.query(Puzzle).filter_by(id = 1):
-            user_input=input(f'{puzzle.question}')
-            for outcome in session.query(Outcome).filter_by(puzzle_id = 1):
-                if user_input == 'Yes':
-                    user_input=input(f'{outcome.name}')
-                if user_input == 'No':
-                    user_input=input(f'{outcome.name}')
-                elif user_input != 'Yes':
-                    print(f'{user_input} is not yes or no')
-    elif user_input != 'x':
-        print(f'{user_input} not an allowed key')
+puzzle_id = 1
+outcome_id = 1
+check = False
+while user_input != 'x' and outcome_id != 11:
+    # if user_input == '':
+        # for puzzle in session.query(Puzzle).filter_by(id = 1):
+    current_puzzle = session.query(Puzzle).filter_by(id = puzzle_id).first()
+    user_input=input(current_puzzle.question)
+    # for outcome in session.query(Outcome).filter_by(puzzle_id = 1):
+    options = session.query(Choice).filter(Choice.puzzle_id == puzzle_id).all()
+    for option in options:
+        check = False
+        if user_input == option.answer:
+            print(f'{option.outcome.name}')
+            puzzle_id = option.outcome.puzzle_id
+            outcome_id = option.outcome.id
+            check = True
+            
+        # elif user_input == '2':
+        #     print(f'{options[0].outcome.name}')
+        #     puzzle_id = options[0].outcome.puzzle_id
+        elif user_input == 'x':
+            break
+        elif user_input == 'Help':
+            break
+    if user_input == 'Help':
+        print(f'your choices are "x" to exit')
+        for option in options:
+            print(f'"{option.answer}"')
+    elif check == False:
+        print(f'{user_input} is not an acceptable choice, type "Help" for options')
+    
+        # else:
+        #     print(f'{user_input} is not "1" or "2"')
+    # elif user_input != 'x':
+    #     print(f'{user_input} not an allowed key')
 
 print('Thanks for playing!')
 
-#ipdb.set_trace()
+ipdb.set_trace()
